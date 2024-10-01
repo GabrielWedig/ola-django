2ebcc17a1aa8d127027028b5233e08d2ebc90a7a
Gabriel Wedig <gabrielwedig2@gmail.com>
2024-09-30 21:55:22 -0300

    WiP

--- ola_django/first_app/urls.py
+++ ola_django/first_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PessoaCreateView, PessoaListView
+from .views import PessoaCreateView, PessoaListView, PessoaUpdateView
 
 urlpatterns = [
     path('cadastrar/', PessoaCreateView.as_view(), name='cadastrar_pessoa'),
+    path('<int:pk>/editar/', PessoaUpdateView.as_view(), name='editar_pessoa'),
     path('listar/', PessoaListView.as_view(), name='listar_pessoas'),
 ]
--- ola_django/first_app/views.py
+++ ola_django/first_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, ListView
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 from .forms import PessoaForm
 from .models import Pessoa
 
@@ -13,4 +13,15 @@ class PessoaCreateView(CreateView):
 class PessoaListView(ListView):
     model = Pessoa
     template_name = 'lista_pessoas.html'
+
+class PessoaUpdateView(UpdateView):
+    model = Pessoa
+    form_class = PessoaForm
+    template_name = 'editar_pessoa.html'
+    success_url = reverse_lazy('listar_pessoas')
+
+class PessoaDeleteView(DeleteView):
+    model = Pessoa
+    # template_name = 'editar_pessoa.html'
+    success_url = reverse_lazy('listar_pessoas')
     
